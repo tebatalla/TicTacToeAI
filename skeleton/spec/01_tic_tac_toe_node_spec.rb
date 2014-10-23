50909e0acaccaab29af60d1b8637c9f56261e76e
Ned Ruggeri <ruggeri@self-loop.com>
2014-10-23 16:42:56 -0700

    update to rspec 3.1.0

--- skeleton/spec/01_tic_tac_toe_node_spec.rb
+++ skeleton/spec/01_tic_tac_toe_node_spec.rb
@@ -25,7 +25,7 @@ describe TicTacToeNode do
     it "all their marks' are the opposite of their parents'" do
       expect(empty_board_node.children.all? do |kid|
         kid.next_mover_mark == :o
-      end).to be_true
+      end).to eq(true)
     end
 
     it "all their #prev_mov_pos values are their parent's" do
@@ -40,15 +40,15 @@ describe TicTacToeNode do
         kid_boards.none? do |kid_board|
         kid_board.object_id == empty_board_node.object_id
         end
-      ).to be_true
+      ).to eq(true)
     end
 
     it "doesn't produce children that include non-empty squares" do
       empty_board_node.board[[0,0]] = :x
       empty_board_node.board[[0,1]] = :o
       kids = empty_board_node.children.map{ |kid| kid.prev_move_pos }
-      expect(kids.include?([0,0])).to be_false
-      expect(kids.include?([0,1])).to be_false
+      expect(kids.include?([0,0])).to eq(false)
+      expect(kids.include?([0,1])).to eq(false)
     end
   end
 
@@ -57,8 +57,8 @@ describe TicTacToeNode do
       empty_board_node.board[[0,0]] = :o
       empty_board_node.board[[0,1]] = :o
       empty_board_node.board[[0,2]] = :o
-      expect(empty_board_node.losing_node?(:o)).to be_false
-      expect(empty_board_node.losing_node?(:x)).to be_true
+      expect(empty_board_node.losing_node?(:o)).to eq(false)
+      expect(empty_board_node.losing_node?(:x)).to eq(true)
     end
 
     let(:loser) do
@@ -68,27 +68,27 @@ describe TicTacToeNode do
       node.board[[0,2]] = :o
       node
     end
-    
+
 		let(:opponent_winner) do
 		  node = TicTacToeNode.new(Board.new, :o)
 			node.board[[0, 0]] = :x
 			node.board[[0, 1]] = :x
 			node.board[[0, 2]] = :o
 			node.board[[1, 1]] = :o
-		  node.board[[1, 0]] = :x	
+		  node.board[[1, 0]] = :x
 			node
 		end
 
     context "when it's the player's turn" do
       it "detects when every child is a loser" do
-        expect(loser.losing_node?(:x)).to be_true
+        expect(loser.losing_node?(:x)).to eq(true)
       end
     end
 
     context "when it's the opponent's turn" do
       it "detects when any child is a loser" do
-        expect(loser.losing_node?(:o)).to be_false
-			  expect(opponent_winner.losing_node?(:x)).to be_true
+        expect(loser.losing_node?(:o)).to eq(false)
+			  expect(opponent_winner.losing_node?(:x)).to eq(true)
       end
     end
   end
@@ -111,19 +111,19 @@ describe TicTacToeNode do
     end
 
     it "detects when the game is already won" do
-      expect(won_node.winning_node?(:o)).to be_false
-      expect(won_node.winning_node?(:x)).to be_true
+      expect(won_node.winning_node?(:o)).to eq(false)
+      expect(won_node.winning_node?(:x)).to eq(true)
     end
 
     context "when it's the player's turn" do
       it "detects when any child is a winner" do
-        expect(winner.winning_node?(:x)).to be_true
+        expect(winner.winning_node?(:x)).to eq(true)
       end
     end
 
     context "when it's the opponent's turn" do
       it "detects when every child is a winner" do
-        expect(winner.winning_node?(:o)).to be_false
+        expect(winner.winning_node?(:o)).to eq(false)
       end
     end
   end
