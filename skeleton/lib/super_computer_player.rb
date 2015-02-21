d137616073c6dac655410e45d48348fc84943ce2
Tomas Batalla <tebatalla@gmail.com>
2015-02-20 19:13:51 -0800

    Solved

--- skeleton/lib/super_computer_player.rb
+++ skeleton/lib/super_computer_player.rb
@@ -1,8 +1,20 @@
 require_relative 'tic_tac_toe_node'
 
+
 class SuperComputerPlayer < ComputerPlayer
+
   def move(game, mark)
+    tree = TicTacToeNode.new(game.board, mark)
+    tree.children.each do |node|
+      return node.prev_move_pos if node.winning_node?(mark)
+    end
+    tree.children.each do |node|
+      return node.prev_move_pos unless node.losing_node?(mark)
+    end
+    raise "what the hell, you cheated!"
   end
+
+
 end
 
 if __FILE__ == $PROGRAM_NAME
--- skeleton/lib/tic_tac_toe_node.rb
+++ skeleton/lib/tic_tac_toe_node.rb
@@ -1,17 +1,63 @@
+require 'byebug'
+
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
-  end
-
-  def losing_node?(evaluator)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos
   end
 
   def winning_node?(evaluator)
+    if @board.over?
+      return @board.winner == evaluator
+    end
+    if self.next_mover_mark == evaluator
+      self.children.any? do |child_node|
+        child_node.winning_node?(evaluator)
+      end
+    else
+      self.children.all? { |child_node| child_node.winning_node?(evaluator) }
+    end
   end
 
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    child_boards = []
+    (0...@board.rows.length).each do |x|
+      (0...@board.rows.length).each do |y|
+        if @board[[x, y]].nil?
+          next_board = @board.dup
+          next_board[[x,y]] = @next_mover_mark
+          next_board_mover_mark = ((@next_mover_mark == :x) ? :o : :x)
+          next_board_node = TicTacToeNode.new(next_board, next_board_mover_mark, [x, y])
+          child_boards << next_board_node
+          # @prev_move_pos = [x, y]
+        end
+      end
+    end
+    child_boards
+  end
+
+
+  def losing_node?(evaluator)
+    if @board.over?
+      if @board.winner.nil?
+        return false
+      elsif @board.winner != evaluator #= ((evaluator == :x) ? :o : :x)
+        return true
+      else
+        return false
+      end
+    end
+
+    if self.next_mover_mark == evaluator
+      self.children.all? { |node| node.losing_node?(evaluator) }
+    else
+      self.children.any? { |node| node.losing_node?(evaluator) }
+    end
   end
 end
